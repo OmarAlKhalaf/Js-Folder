a3c5cd45bada597e441b7efc98fadc657be10b64
OmarAlKhalaf <99047548@mydavinci.nl>
2022-06-15 13:24:51 +0200

    (refresh)

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ btn.addEventListener('click',func)
 // func here onder.
 function func(){
     if(x == 0){
-        document.body.style.backgroundColor = 'blue'
+        document.body.style.backgroundColor = 'yellow'
         btn.style.backgroundColor = 'white'
         console.log('On')
-        btn.style.color = 'white'
+        btn.style.color = 'black'
         x++
     }
     else if (x == 1){
-        document.body.style.backgroundColor = 'red'
+        document.body.style.backgroundColor = 'black'
         btn.style.backgroundColor = 'white'
-        btn.style.color = 'white'
+        btn.style.color = 'black'
         console.log('Off')
         x-- 
     }
